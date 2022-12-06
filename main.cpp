b20f55fdb55240b78c0a140f1f1cd38908a5a272
newmomizi <114981625+newmomizi@users.noreply.github.com>
2022-12-06 00:27:17 +0000

    Made i-number detector.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
-#include<iostream>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
 using namespace std;
 
 int main() {
-  cout << "Hello, World!" << endl;
-  return 0;
+  vector<bool> vec(100000000);
+  vec.at(1) = true;
+  int i;
+  int k;
+  for(i=1; i<100000000; i++){
+    vec.at(i) = false;
+  }
+
+  for(i=0; i<10000; i++) {
+    if (vec.at(i)==false) {
+      k=i+i;
+      while(k <= 10000000) {
+        vec.at(k) = true;
+        k += i;
+      }
+    }
+  }
+
+  for(i=0; i<100000000; i++) {
+    if (vec.at(i) == false) {
+      cout << i << endl; 
+    }
+  }
 }
